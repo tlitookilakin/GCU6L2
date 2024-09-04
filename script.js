61aa959255cacb6fb37a2806dc22a37872560da0
Wren <grahamgrasley@gmail.com>
2024-09-04 15:36:29 -0400

    Finish specification, styling

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-function update(index, items) {
+function updatePlanetDisplay(index, items) {
     document.getElementById("counter").innerText = index + 1;
     let planet = items[index];
 
     document.querySelector("#content h2").innerText = planet.name;
-    document.querySelector("#content h3").innerText = planet.inner ? "Inner planet" : "Outer planet";
+    document.querySelector("#content p").innerText = planet.inner ? "Inner planet" : "Outer planet";
 
     let planet_display = document.getElementById("planet");
     planet_display.style.backgroundColor = planet.color;
-    planet_display.style.transform = `scale(${planet.size / 60})`
+    planet_display.style.transform = `scale(${planet.size / 100})`
+
+    if (!getWrappingEnabled()) {
+        document.getElementById("prev").disabled = (index === 0);
+        document.getElementById("next").disabled = (index === (items.length - 1));
+    }
+    else {
+        document.getElementById("prev").disabled = false;
+        document.getElementById("next").disabled = false;
+    }
+}
+
+function getAverageDiameter(items) {
+    let sum = 0;
+    for (let item of items) {
+        sum += item.size;
+    }
+    return sum / items.length;
+}
+
+function getWrappingEnabled() {
+    return document.getElementById("wrapEnabled").checked;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -22,6 +43,11 @@ document.addEventListener("DOMContentLoaded", () => {
         {name: "Neptune", inner: false, size: 30599, color : "#456eff"}
     ];
 
+    planets.forEach(n => console.log(n.name));
+    let avg = getAverageDiameter(planets);
+    console.log(avg);
+    
+
     let index = 0;
 
     document.getElementById("prev").addEventListener("click", () => {
@@ -29,13 +55,14 @@ document.addEventListener("DOMContentLoaded", () => {
         if (index < 0 ) {
             index = planets.length + (index % planets.length);
         }
-        update(index, planets);
+        updatePlanetDisplay(index, planets);
     });
     
     document.getElementById("next").addEventListener("click", () => {
         index = (index + 1) % planets.length;
-        update(index, planets);
+        updatePlanetDisplay(index, planets);
     });
 
-    update(0, planets);
+    document.getElementById("wrapEnabled").addEventListener("change", () => updatePlanetDisplay(index, planets));
+    updatePlanetDisplay(0, planets);
 });
